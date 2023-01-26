accc7405d20c88d37fb444c12931064fdb249226
coopsie <aislingkcooper@hotmail.com>
2023-01-26 16:03:24 +0000

    edit class notes

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,9 +5,10 @@ class Movie {
         this.director = inputDirector;
     };
     async create (movieCollection) {
-        console.log("Entering add within index.js"); // this console.log is for debugging purposes, can delete later
+        console.log("Entering add within index.js"); 
+        // This console.log is for debugging purposes, can delete later
         await movieCollection.insertOne(this);
-        //code to save a movie to the database here
+        // Code to save a movie to the database here
     };
 };
 
